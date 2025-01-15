14219ef4f52ea975f782309d2621be3c97731c56
Thanajade Dechananthachai <thanajade.dechananthachai@krungsrinimble.com>
2025-01-15 22:10:28 +0700

    update Jenkins setup script to use correct home directory and install Docker and Git

--- jenkins/jenkins-cmd.sh
+++ jenkins/jenkins-cmd.sh
@@ -2,15 +2,9 @@
 
 mkdir -p /home/ec2-user/jenkins
 
-docker run -d --name=jenkins jenkins/jenkins:lts-jdk17 \
-  -u 0 \
-  -p 8080:8080 \
-  -p 50000:50000 \
-  -v /home/ec2-user/jenkins:/var/jenkins_home
-
 docker run --name=jenkins \
   --user root \
-  -d --privileged -v /data/jenkins:/var/jenkins_home \
+  -d --privileged -v /home/ec2-user/jenkins:/var/jenkins_home \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -p 8080:8080 \
   -p 50000:50000 \
@@ -18,3 +12,8 @@ docker run --name=jenkins \
   jenkins/jenkins:lts-jdk17
 
 docker exec -it -u root jenkins bash
+
+apt-get update
+apt-get install -y docker.io
+
+apt-get install -y git
